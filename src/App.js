a9398c9b63f4be4bbdd371751dff3f3828fdd7f0
Jose Fletes <joseangelfletes@gmail.com>
2022-11-11 12:29:22 -0800

    Set routes to Home, StudentList and ProgramList

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import StudentList from "./components/StudentList";
+import ProgramList from "./components/ProgramList";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={ <Home /> } />
+          <Route exact path="/students" element={ <StudentList /> } />
+          <Route exact path="/programs" element={ <ProgramList /> } />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
-
-const router = createBrowserRouter([{
-  path: "/",
-  element: <div>Hello world!</div>,
-},
-])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
