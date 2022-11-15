e06d0d3ddd0e545609a773e8cf22a5684155bbdc
Jose Fletes <joseangelfletes@gmail.com>
2022-11-15 01:18:45 -0800

    Add links to Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import './Styles/Home.css'
 
 const Home = () => {
   return (
     <div className='home'>
       <h1>Welcome to Music & Art School</h1>
+      <li>To view our Programs, click <NavLink to="/programs">here</NavLink></li>
+      <li>To view our Students, click <NavLink to="/students">here</NavLink></li>
+      <li>To register a Student, click <NavLink to="/students/new">here</NavLink></li>
+      <li>To add a new Program, click <NavLink to="/programs/new">here</NavLink></li>
     </div>
   )
 }
